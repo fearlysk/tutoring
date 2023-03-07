cff6da635b02e73fc20ddf8b6dfa2abf72cee7dc
fearlysk <sqleye1@mail.ru>
2023-03-07 22:16:39 +0300

    add messaging to telegram

--- src/components/UI/icons/IconRight.jsx
+++ src/components/UI/icons/IconRight.jsx
@@ -1,7 +1,7 @@
 const IconRight = () => {
     return ( 
     <svg width="20" height="36" viewBox="0 0 32 57" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M2 2L28.5 29.3281L2 55" stroke="white" stroke-width="4"/>
+        <path d="M2 2L28.5 29.3281L2 55" stroke="white" strokeWidth="4"/>
     </svg>
     )
 }
